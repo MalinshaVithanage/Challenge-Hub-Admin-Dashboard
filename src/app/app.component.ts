a85c8db7cb973eb454d25e18991ffb9c7af02f5f
malinshav <malikgvitha@gmail.com>
2024-03-27 14:15:48 +0530

    Finished Data Pass
    successfully pass data and store in firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layouts/header/header.component";
 import { FooterComponent } from "./layouts/footer/footer.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { CategoriesComponent } from './categories/categories.component';
+import { FirebaseApp } from '@angular/fire/app';
+import { Firestore, collection, getDocs } from '@angular/fire/firestore';
 // import {AngularFireModule} from '@angular/fire';
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent, FooterComponent, DashboardComponent]
+    imports: [RouterOutlet, HeaderComponent, FooterComponent, DashboardComponent, ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'server';
+  // firestore = inject(Firestore);
+
+  ngOnInit() {
+    // getDocs(collection(this.firestore, "testPath")).then((response) => {
+    //   console.log(response.docs)
+    // }
+  // )
+}
+
 }
--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,12 +1,96 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { DocumentReference, Firestore, addDoc } from '@angular/fire/firestore';
+import { FormsModule, NgForm } from '@angular/forms';
+import { error, log } from 'console';
+// import {AngularFirestore} from '@angular/fire/firestore';
+import { collection, getDocs } from '@angular/fire/firestore';
+import { doc } from 'firebase/firestore';
+// import { FirebaseFirestore } from '@angular/fire/firestore';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.css'
 })
 export class CategoriesComponent {
+  constructor(private afs:Firestore){}
+  // afs = inject(Firestore);
+
+  ngOnInit():void{
+    
+  }
+  onSubmit(formData:any) {
+    let categoryData = {
+      category: formData.value.category
+    }
+    let subCategoryData = {
+      subCategory:"subCategory1"
+    }
+    let subSubCategoryData = {
+      subCategory:"subSubCategory1"
+    }
+    // this.afs.collection('categories').add(categoryData).then((DocumentReference: any) => {
+    //   console.log(DocumentReference);
+      
+    // }).catch((err: any) =>{
+    //   console.log(err);
+      
+    // })
+  //   getDocs(collection(this.afs, "categories")).then((response) => {
+  //     console.log(response.docs)
+  //   }
+  // )
+//   getDocs(collection(this.afs, "categories")).then((response) => {
+//   console.log(response);
+// }).catch((err) => {
+//   console.error("Error fetching documents:", err);
+// });
+//   }
+
+// this.afs.collection('categories').add(categoryData).then(docRef => {
+//   console.log(docRef);
+
+//   this.afs.collection('categories').doc(docRef.id).collection('subCategories').add(subCategoryData).then(docRef1 => {
+//     console.log(docRef1);
+    
+//     this.afs.collection('categories').doc(docRef.id).collection('subCategories').doc(docRef1.id).collection('subSubCategories').add(subSubCategoryData).then(docRef2 => {
+//       console.log('Second Level Subcategory daved successfully');
+      
+//     })
+//   })
+//   formData.resetForm();
+// })
+// .catch((error) => {
+//   console.error('Error adding category: ', error);
+// });
+// }
+
+
+
+addDoc(collection(this.afs, 'categories'), categoryData)
+      .then((docRef) => {
+        console.log('Category added with ID: ', docRef.id);
+        console.log(docRef);
+
+        addDoc(collection(this.afs, 'categories', docRef.id, 'subCategories'), subCategoryData)
+      .then((docRef1) => {
+        console.log(docRef1);
+
+        // this.afs.doc(`categories/${docRef.id}/subCategories/${docRef1.id}`).collection('subSubCategories').add(subCategoryData)
+
+        addDoc(collection(this.afs, 'categories', docRef.id, 'subCategories',  docRef1.id, 'subSubCategories'), subSubCategoryData)
+      .then((docRef2) => {
+        console.log("Second Level Subcategory saved successfully");
+      })
+      })
+        formData.resetForm();
+      })
+      .catch((error) => {
+        console.error('Error adding category: ', error);
+      });
+  }
+  
 
 }
