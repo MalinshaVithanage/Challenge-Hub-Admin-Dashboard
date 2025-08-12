a335b581c3cd99e0fb9a18087b7f813b52d576fb
Malinsha Vithanage <86399536+MalinshaVithanage@users.noreply.github.com>
2025-08-12 16:25:19 +0530

    new post created

--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-new-post',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './new-post.component.html',
   styleUrl: './new-post.component.css'
 })
