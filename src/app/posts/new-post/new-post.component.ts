ef8dd432fab23eca0458d57cda04fe47970313e2
Malinsha Vithanage <86399536+MalinshaVithanage@users.noreply.github.com>
2025-08-14 16:16:34 +0530

    created permalink

--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-new-post',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './new-post.component.html',
   styleUrl: './new-post.component.css'
 })
 export class NewPostComponent {
 
+  permalink: string = '';
+
+  onTitleChanged(event: any) {
+    const title = event.target.value;
+    this.permalink = title.replace(/\s/g, '-'); 
+   
+  }
 }
