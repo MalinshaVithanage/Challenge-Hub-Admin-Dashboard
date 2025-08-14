201c2642e725583a100fcc18e017a214e1a7d650
Malinsha Vithanage <86399536+MalinshaVithanage@users.noreply.github.com>
2025-08-14 17:13:05 +0530

    post Image completed

--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -12,10 +12,19 @@ import { RouterLink } from '@angular/router';
 export class NewPostComponent {
 
   permalink: string = '';
+  imgSrc: string = './assets/placeholder-image.jpg';
+  selectedImg:any;
 
   onTitleChanged(event: any) {
     const title = event.target.value;
     this.permalink = title.replace(/\s/g, '-'); 
-   
+  }
+  showPreview(event: any) {
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      this.imgSrc = e.target.result;
+    } 
+    reader.readAsDataURL(event.target.files[0]);
+    this.selectedImg = event.target.files[0];
   }
 }
