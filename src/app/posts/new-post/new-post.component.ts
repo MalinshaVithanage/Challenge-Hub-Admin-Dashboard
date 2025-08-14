d16239439236c5e50d9afb81941322571aa1f37c
Malinsha Vithanage <86399536+MalinshaVithanage@users.noreply.github.com>
2025-08-14 22:57:26 +0530

    added text editor

--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { CategoriesService } from '../../services/categories.service';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularEditorModule } from '@kolkov/angular-editor';
+
 
 @Component({
   selector: 'app-new-post',
   standalone: true,
-  imports: [RouterLink, FormsModule],
+  imports: [RouterLink, FormsModule, CommonModule, HttpClientModule, AngularEditorModule],
   templateUrl: './new-post.component.html',
   styleUrl: './new-post.component.css'
 })
@@ -15,6 +20,16 @@ export class NewPostComponent {
   imgSrc: string = './assets/placeholder-image.jpg';
   selectedImg:any;
 
+  categories: any[] = [];
+
+  constructor(private categoryService: CategoriesService) { }
+
+  ngOnInit():void {
+    this.categoryService.loadData().subscribe(val => {
+      this.categories = val;
+    });
+
+  }
   onTitleChanged(event: any) {
     const title = event.target.value;
     this.permalink = title.replace(/\s/g, '-'); 
