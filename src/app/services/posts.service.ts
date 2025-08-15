7bb3fb1ef6ace5af3d1299cbdc5db75a378317cb
Malinsha Vithanage <86399536+MalinshaVithanage@users.noreply.github.com>
2025-08-16 00:54:55 +0530

    edit post initially completed

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -7,53 +7,73 @@ import { collection, getDocs } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PostsService {
-  constructor(private storage: Storage,
+  constructor(
+    private storage: Storage,
     private afs: AngularFirestore,
     private toastr: ToastrService,
-    private router: Router,
-  ) { }
+    private router: Router
+  ) {}
 
-  async uploadImage(selectedImage: File, postData: any) {
+  async uploadImage(selectedImage: File, postData: any, formStatus: string, id: string) {
     try {
       const filePath = `postIMG/${Date.now()}`;
       console.log('File path:', filePath);
 
       const storageRef = ref(this.storage, filePath);
 
-      // Upload the file
       await uploadBytes(storageRef, selectedImage);
 
       console.log('Post image uploaded successfully');
-      const downloadURL = await import('@angular/fire/storage').then(m => m.getDownloadURL(storageRef));
+      const downloadURL = await import('@angular/fire/storage').then((m) =>
+        m.getDownloadURL(storageRef)
+      );
       postData.postImgPath = downloadURL;
+
+      if (formStatus === 'Edit') {
+        this.updateData(id, postData);
+      }else{
       console.log(postData);
-      this.afs.collection('posts').add(postData).then((docRef) => {
-        this.toastr.success('Data Inserted Successfully');
-       this.router.navigate(['/posts']);
-      })
-        
+      this.afs
+        .collection('posts')
+        .add(postData)
+        .then((docRef) => {
+          this.toastr.success('Data Inserted Successfully');
+          this.router.navigate(['/posts']);
+        });
+      }
+     
     } catch (error) {
       console.error('Error uploading image:', error);
     }
   }
 
-    loadData(): Observable<any[]> {
-      return new Observable<any[]>((observer) => {
-        getDocs(collection(this.afs.firestore, 'posts'))
-          .then((querySnapshot) => {
-            const data: any[] = [];
-            querySnapshot.forEach((doc) => {
-              data.push({ id: doc.id, data: doc.data() });
-            });
-            observer.next(data);
-            observer.complete();
-          })
-          .catch((error) => {
-            observer.error(error);
+  loadData(): Observable<any[]> {
+    return new Observable<any[]>((observer) => {
+      getDocs(collection(this.afs.firestore, 'posts'))
+        .then((querySnapshot) => {
+          const data: any[] = [];
+          querySnapshot.forEach((doc) => {
+            data.push({ id: doc.id, data: doc.data() });
           });
-      });
-    }
+          observer.next(data);
+          observer.complete();
+        })
+        .catch((error) => {
+          observer.error(error);
+        });
+    });
+  }
+
+  loadOneData(id: string) {
+    return this.afs.doc(`posts/${id}`).valueChanges();
+  }
+  updateData(id: string, postData: any) {
+    this.afs.doc(`posts/${id}`).update(postData).then(() => {
+      this.toastr.success('Post Updated Successfully');
+      this.router.navigate(['/posts']);
+    })
+  }
 }
