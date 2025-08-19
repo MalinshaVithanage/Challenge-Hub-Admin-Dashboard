18a5e1b6807aef900a4a466351aa26c9e58f63ef
Malinsha Vithanage <86399536+MalinshaVithanage@users.noreply.github.com>
2025-08-19 23:49:08 +0530

    login validation done

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,13 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { FormsModule } from '@angular/forms';
+import { CONNREFUSED } from 'dns';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, AngularFireAuthModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+onSubmit(formValue: any) {
+    console.log('Form Submitted!', formValue);
+}
 }
